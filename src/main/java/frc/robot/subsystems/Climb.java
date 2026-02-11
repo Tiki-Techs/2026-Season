93aa580184aaa1f67bf535d48674482402de080a
Daenaq <bobthedabber7@gmail.com>
2026-02-10 18:59:37 -1000

    auto climb code;

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems;
 
+import java.util.function.DoubleSupplier;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -13,8 +16,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Climb extends SubsystemBase {
 
     private final TalonFX climbMotor = new TalonFX(30);
+    private final DigitalInput lowerLimitSwitch = new DigitalInput(4);
+    private final DigitalInput upperLimitSwitch = new DigitalInput(5);
 
-    private final DutyCycleOut dutyCycleRequest = new DutyCycleOut(0);
+
 
 
     public Climb() {
@@ -22,29 +27,79 @@ public class Climb extends SubsystemBase {
         config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         climbMotor.getConfigurator().apply(config);
         climbMotor.setPosition(0);
+        
     }
 
-    // public Command climbUp(DoubleSupplier speed) {
-    //     return new RunCommand(() ->
-    //         climbMotor.setControl(dutyCycleRequest.withOutput(speed)), this);
+    public Command climbUpManual(double speed) {
+        return new RunCommand(() ->
+            climbMotor.set(speed), this);
+    }
+
+    public Command climbDownManual(double speed) {
+        return new RunCommand(() ->
+            climbMotor.set(-speed), this);
+    }
+
+    // public Command climbUpAuto(){
+    //     return new RunCommand(() -> {
+    //         if (!upperLimitSwitch.get()){
+    //             climbMotor.set(0.1);
+    //         } else {
+    //             climbMotor.set(0);
+    //         }
+
+    //     });
     // }
 
-    // public Command climbDown(DoubleSupplier speed) {
-    //     return new RunCommand(() ->
-    //         climbMotor.setControl(dutyCycleRequest.withOutput(speed)), this);
+    // public Command climbDownAuto(){
+    //     return new RunCommand(() -> {
+    //         if (!lowerLimitSwitch.get()){
+    //             climbMotor.set(-0.1);
+    //         } else {
+    //             climbMotor.set(0);
+    //         }
+
+    //     });
     // }
+
+    public Command climbDownAuto(){
+        return new RunCommand(() -> {
+            climbMotor.set(-0.1);
+            }, this).until(()->!lowerLimitSwitch.get())
+            .finallyDo(()->climbMotor.set(0));
+    }
+
+    public Command climbUpAuto(){
+        return new RunCommand(() -> {
+            climbMotor.set(0.1);
+            }, this).until(()->!upperLimitSwitch.get())
+            .finallyDo(()->climbMotor.set(0));
+    }
 
     public Command stopAll() {
         return new RunCommand(() ->
-            climbMotor.setControl(dutyCycleRequest.withOutput(0)), this);
+            climbMotor.set(0), this);
     }
 
     public double getPosition() {
         return climbMotor.getPosition().getValueAsDouble();
     }
 
+    public Command resetPosition() {
+        return new RunCommand(() -> {
+            climbMotor.set(-0.1);
+        }, this).until(()->lowerLimitSwitch.get())
+        .andThen(()->{
+        climbMotor.set(0);
+        climbMotor.setPosition(0);
+        });
+    }
+
+
     @Override
     public void periodic() {
         SmartDashboard.putNumber("Climb/Position", getPosition());
+        SmartDashboard.putBoolean("Climb Lower Limit", lowerLimitSwitch.get());
+        SmartDashboard.putBoolean("Climb Upper Limit", upperLimitSwitch.get());
     }
 }
